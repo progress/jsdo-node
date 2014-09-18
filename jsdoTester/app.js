5c016392c94620b2c44c1df54c926744445427d3
mendoncakeegan <mendoncakeegan@gmail.com>
2014-09-18 14:35:14 -0700

    Cleaned up formatting

--- jsdoTester/app.js
+++ jsdoTester/app.js
@@ -2,8 +2,6 @@
 // Progress Software
 // August 2014
 // app.js
-
-
 var express = require('express');
 var http = require('http');
 var cors = require('cors');
@@ -25,40 +23,42 @@ var catalogURI = "YOUR URL HERE e.g. http://nodejsdo-21944.onmodulus.net/catalog
 
 var server = http.createServer(app);
 
-server.listen(app.get('port'), function()
-{
+server.listen(app.get('port'), function() {
     console.log('express server listening on port ' + app.get('port'));
 });
 
 // This should display the customer data. If no data is shown, you can use the console.log(#) to see where it stopped working.
-app.get('/test', function(req, res)
-{
+app.get('/test', function(req, res) {
     session = new progress.data.Session();
     console.log('1.1');
     session.login(serviceURI, "", "");
     console.log('1.2');
     session.addCatalog(catalogURI);
     console.log('2');
-    jsdo = new progress.data.JSDO({ name: 'dsCustomer' });
+    jsdo = new progress.data.JSDO({
+        name: 'dsCustomer'
+    });
     jsdo.subscribe('AfterFill', onAfterFillCustomers, this);
     jsdo.fill(); // fills the locally initialized jsdo from the catalog
     var arr = [];
     console.log('3');
-    function onAfterFillCustomers(jsdo, success, request)
-    {
+
+    function onAfterFillCustomers(jsdo, success, request) {
         console.log('4');
-        jsdo.eCustomer.foreach(function(customer)
-        {
-            arr.push({name: jsdo.eCustomer.Name, address: jsdo.eCustomer.Address,
-                        city: jsdo.eCustomer.City, state: jsdo.eCustomer.State});
+        jsdo.eCustomer.foreach(function(customer) {
+            arr.push({
+                name: jsdo.eCustomer.Name,
+                address: jsdo.eCustomer.Address,
+                city: jsdo.eCustomer.City,
+                state: jsdo.eCustomer.State
+            });
         });
         console.log(arr);
         res.write(JSON.stringify(arr));
         res.end();
         console.log('5');
     }
- });
-app.get('/', function(req, res){
-  res.render('main');
-}
-);
+});
+app.get('/', function(req, res) {
+    res.render('main');
+});
--- jsdoTester/public/rest.js
+++ jsdoTester/public/rest.js
@@ -1,16 +1,16 @@
-function App($scope, $http)
-{
-	$http({method: 'GET', url: '/test'}).
-	success(function(data)
-	{
-		console.log('good');
-		$scope.companies = data;
-		console.log(data);
-	}).
-	error(function(data)
-	{
-		console.log('bad');
-		$scope.companies = 'error';
-		console.log(data);
-	});
+function App($scope, $http) {
+    $http({
+        method: 'GET',
+        url: '/test'
+    }).
+    success(function(data) {
+        console.log('good');
+        $scope.companies = data;
+        console.log(data);
+    }).
+    error(function(data) {
+        console.log('bad');
+        $scope.companies = 'error';
+        console.log(data);
+    });
 }
